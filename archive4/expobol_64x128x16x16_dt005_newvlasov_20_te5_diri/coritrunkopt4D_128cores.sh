d8f09bff092ec172f653531e3329dfe3b4623af8
Wonjae Lee <wol023@ucsd.edu>
2016-03-06 16:46:35 -0800

    new files from nersc

--- archive4/expobol_64x128x16x16_dt005_newvlasov_20_te5_diri/coritrunkopt4D_128cores.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-#SBATCH --partition debug
-#SBATCH --nodes 4
-#SBATCH --time=00:30:00
-
-cd $SLURM_SUBMIT_DIR   # optional, since this is the default behavior
-srun -n 128 ../CORIcogenttrunk.ex kinetic.in
