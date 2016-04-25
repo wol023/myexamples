308c5b938258f6cf3f06d218c717930d2e7d5136
Wonjae Lee <wol023@ucsd.edu>
2016-04-25 10:55:47 -0700

    update from x220

---
+++ 5D/blob3D/runx220devel5Ddbg5D_beforemerge.sh
@@ -0,0 +1,2 @@
+/home/wonjae/ws/branchinESL/devel5Ddbg5D_beforemerge/exec/cogent.Linux.64.mpicxx.gfortran.DEBUG.MPI.ex  kinetic.in |tee old.txt
+
--- 5D/gamsmall3dslab/testrun.sh
+++ 5D/gamsmall3dslab/testrun.sh
@@ -6,5 +6,6 @@ echo "rm -v pout.*"
 rm -v pout.*
 
 # gam test
-mpirun -np 1 /home/wonjae/ws/branchinESL/devel5D/exec/cogent.Linux.64.mpicxx.gfortran.OPT.MPI.ex  GamSmall.in |tee output.txt
+mpirun -np 1 /home/wonjae/ws/branchinESL/devel5Ddbg5D_beforemerge/exec/cogent.Linux.64.mpicxx.gfortran.DEBUG.MPI.ex  GamSmall.in |tee output.txt
+#mpirun -np 1 /home/wonjae/ws/branchinESL/devel5Ddbg5D/exec/cogent.Linux.64.mpicxx.gfortran.OPT.MPI.ex  GamSmall.in |tee output.txt
 
