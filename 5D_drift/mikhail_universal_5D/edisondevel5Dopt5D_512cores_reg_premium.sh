36eac815f3e5b3517958e24067db65fd20902fa6
Wonjae Lee <wol023@ucsd.edu>
2016-06-30 16:03:33 -0700

    from edison

---
+++ 5D_drift/mikhail_universal_5D/edisondevel5Dopt5D_512cores_reg_premium.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+#SBATCH --partition regular
+#SBATCH --qos=premium
+#SBATCH --nodes 22
+#SBATCH --time=03:30:00
+
+cd $SLURM_SUBMIT_DIR   # optional, since this is the default behavior
+srun -n 512 ../../EDISONcogent5Dopt5D.ex kinetic.in
