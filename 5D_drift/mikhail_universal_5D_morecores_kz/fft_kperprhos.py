18cb258539711167e21c16576acfd23e44c75d36
Wonjae Lee <wol023@ucsd.edu>
2016-08-01 15:28:11 -0700

    omega analysis fftkperprhos.py

--- 5D_drift/mikhail_universal_5D_morecores_kz/fft_kperprhos.py
+++ 5D_drift/mikhail_universal_5D_morecores_kz/fft_kperprhos.py
@@ -208,7 +208,7 @@ transformations = (standard_transformations + (implicit_multiplication_applicati
 pe=parse_expr(n0_grid_func,transformations=transformations)
 #print 'parsed expression = ',pe
 
-f = lambdify((x,y),pe)
+f = lambdify((x,y,z),pe)
 #print f(pi,pi) #test
 
 xx = np.linspace(0.0, np.pi*2, (x_cells))
@@ -217,7 +217,7 @@ yy = np.linspace(0.0, np.pi*2, (x_cells))
 yypert = np.linspace(0.0, np.pi*2, (x_cells))
 
 for i in range(len(xx)):
-    yy[i] = f(xx[i],pi)
+    yy[i] = f(xx[i],0,0)
 
 #calc yypert
 dphasepert = np.pi/10
@@ -227,13 +227,13 @@ maxyypert_phi_ind =0
 
 for j in range(10):
     for i in range(len(xx)):
-        yypert[i] =abs(f(xx[i],dphasepert*j) - yy[i])
+        yypert[i] =abs(f(xx[i],dphasepert*j,0) - yy[i])
     tempyypert_amp = max( (yypert))
     if tempyypert_amp>maxyypert_amp:
         maxyypert_amp=tempyypert_amp
         maxyypert_phi_ind=j
 for i in range(len(xx)):
-    yypert[i] =abs(f(xx[i],dphasepert*maxyypert_phi_ind) - yy[i])
+    yypert[i] =abs(f(xx[i],dphasepert*maxyypert_phi_ind,0) - yy[i])
 
 
 dx = x_max*100/len(xx) #in [cm]
@@ -271,7 +271,7 @@ elif contains_pert:
     ispread_width=sum(yypert>yypert_amplitude*0.10)
 else:
     ispread_width=1
-    print "does not contain spreading"
+    #print "does not contain spreading"
 
 print '********** DERIVED VARS *****************'
 b_t = bz_inner*1E4
@@ -304,6 +304,7 @@ print 'c_elec_gyroradius         [cm] = ', c_elec_gyroradius, '   (/ref: ', c_el
 
 
 k_y        = 2.0*np.pi*m_y/(y_max*100)
+k_x        = 2.0*np.pi*0.5/(x_max*100) 
 deltaL_max = 1./max(abs(dlnyydx))
 x_point_index_in_plot = int(float(x_index)/float(x_cells)*len(dlnyydx))-1 
 #print 'x_point_index_in_plot = ', x_point_index_in_plot
@@ -330,25 +331,25 @@ spread_ind =  spread_ind_diff+ x_point_index_in_plot
 
 c_s        = 979000*((electron_temperature*units_temperature)/ion_mass)**0.5
 rho_s      = 102*((ion_mass*electron_temperature*units_temperature)**0.5)/b_t
-chi        = k_y*rho_s
+chi        = sqrt(k_x*k_x+k_y*k_y)*rho_s
 omega_star = c_s*rho_s*k_y/deltaL_max
 omega_star_point= c_s*rho_s*k_y/ deltaL_point
 omega_star_spread= c_s*rho_s*k_y/ deltaL_spread
 
 print 'k_y             [1/cm] = ', k_y
+print 'k_x             [1/cm] = ', k_x , 'check m_y = (',m_y,') with kinetic.in'
 print 'deltaL_max        [cm] = ', deltaL_max
 print 'deltaL_point      [cm] = ', deltaL_point
 print 'deltaL_spread     [cm] = ', deltaL_spread
 print 'c_s             [cm/s] = ', c_s
 print 'rho_s             [cm] = ', rho_s
-print 'k_y*rho_s          [-] = ', chi
+print 'k_y*rho_s          [-] = ', k_y*rho_s
 print 'k_y*rho_i          [-] = ', k_y*c_ion_gyroradius
+print 'k_perp*rho_s       [-] = ', chi
+print 'k_perp*rho_i       [-] = ', chi/rho_s*c_ion_gyroradius
 print 'omega*           [1/s] = ', omega_star
 print 'omega*_point     [1/s] = ', omega_star_point
 print 'omega*_spread    [1/s] = ', omega_star_spread
-
-
-
 
 print '*****************************************'
 
@@ -371,14 +372,15 @@ plt.gca().legend(bbox_to_anchor = (0.0, 0.1))
 plt.tight_layout()
 plt.savefig('foo1.png')
 plt.savefig('foo1.eps')
-plt.close()
+plt.close('all')
+#plt.clf()
 
 ######
 init_plotting()
 plt.subplot(111)
 plt.gca().margins(0.1, 0.1)
 plt.plot(xcm,-dlnyydx,linestyle='-',linewidth=1,color='b',label='inverse gradient length' )
-plt.plot(xcm,yypert*10000 ,marker='x',linestyle='-',linewidth=1,color='r',label='perturbationx10000' )
+plt.plot(xcm,yypert*10000 ,marker='.',linestyle='-',linewidth=1,color='r',label='perturbationx10000' )
 plt.scatter(xcm[spread_ind],-dlnyydx[spread_ind],label='average points' )
 plt.xlabel(u'x (cm)')
 plt.ylabel(u'perturbation, -d(ln n)/dx [cm]')
@@ -386,7 +388,8 @@ plt.gca().legend(bbox_to_anchor = (0.0, 0.15))
 plt.tight_layout()
 plt.savefig('foo2.png')
 plt.savefig('foo2.eps')
-plt.close()
+plt.close('all')
+#plt.clf()
 
 #read history file
 x_list=[]
@@ -436,6 +439,7 @@ x_list[:] = [i*ref_time/2.0/np.pi for i in x_list]
 N = len(x_list)
 T = (x_list[len(x_list)-1]-x_list[0]) / N
 xt = np.array(x_list)
+dimensional_xt = xt*2.0*np.pi
 y = np.array(y_list)
 
 yf = scipy.fftpack.fft(y)
@@ -455,7 +459,7 @@ dphase = np.pi/100
 yvydiff2 = np.zeros(100)
 for i in range(100):
     phase=dphase*i
-    yv = np.real(y.max()*np.exp(abs(freqmax)*1.j*2.0*np.pi*xt+1.j*phase) )
+    yv = np.real(y.max()*np.exp(abs(freqmax)*1.j*dimensional_xt+1.j*phase) )
     temperror=0.0
     for j in range(len(yv)):
         temperror = temperror+ (yv[j]-y[j])**2
@@ -463,7 +467,7 @@ for i in range(100):
 
 minphaseind=np.abs(yvydiff2).argmin()
 #print minphaseind
-yv = np.real(y.max()*np.exp(abs(freqmax)*1.j*2.0*np.pi*xt+1.j*dphase*minphaseind) )
+yv = np.real(y.max()*np.exp(abs(freqmax)*1.j*dimensional_xt+1.j*dphase*minphaseind) )
 
 
 yfv = scipy.fftpack.fft(yv)
@@ -485,15 +489,15 @@ guess_freq = freqmax
 guess_lin = 0
 
 # we'll use this to plot our first estimate. This might already be good enough for you
-data_first_guess = guess_amplitude*np.cos(guess_freq*2.0*np.pi*xt+guess_phase)+guess_mean
+#data_first_guess = guess_amplitude*np.cos(guess_freq*dimensional_xt+guess_phase)+guess_mean
 
 # Define the function to optimize, in this case, we want to minimize the difference
 # between the actual data and our "guessed" parameters
-optimize_func = lambda z: z[3]*np.cos(z[0]*2.0*np.pi*xt+z[1]) + z[2] - y
-#optimize_func = lambda z: guess_amplitude*np.cos(z[0]*2.0*np.pi*xt+z[1]) + z[2] - y
+optimize_func = lambda z: z[3]*np.cos(z[0]*dimensional_xt+z[1]) + z[2] - y
+#optimize_func = lambda z: guess_amplitude*np.cos(z[0]*dimensional_xt+z[1]) + z[2] - y
 est_freq, est_phase, est_mean, est_amplitude = leastsq(optimize_func, [guess_freq, guess_phase, guess_mean, guess_amplitude ])[0]
 
-data_fit = est_mean + est_amplitude*np.cos(est_freq*2.0*np.pi*xt+est_phase) 
+data_fit = est_mean + est_amplitude*np.cos(est_freq*dimensional_xt+est_phase) 
 # recreate the fitted curve using the optimized parameters
 
 #guess_amplitude = guess_amplitude-est_lin*(xt[len(xt)-1]-xt[0])
@@ -522,18 +526,15 @@ yplotv_fit = scipy.fftpack.fftshift(yfv_fit)
 
 ############
 
-print 'omega_star_fft/omega*            = ', abs(freqmax)/omega_star
-print 'omega_star_fft/omega*_point      = ', abs(freqmax)/ omega_star_point
-print 'omega_star_fitting/omega*        = ', abs(est_freq)/omega_star
-print 'omega_star_fitting/omega*_point  = ', abs(est_freq)/omega_star_point
-print 'omega_star_fitting/omega*_spread = ', abs(est_freq)/omega_star_spread
+
+
 
 init_plotting()
 plt.subplot(111)
 plt.gca().margins(0.1, 0.1)
-plt.plot(xt,y,marker='x',linestyle='-',linewidth=1,color='b',label='potential' )
-#plt.plot(xt,yv,linestyle='-',linewidth=1,color='r',label='fourier mode' )
-plt.plot(xt,data_fit,marker='.',linestyle='-',linewidth=1,color='g',label='fitting' )
+plt.plot(dimensional_xt,y,marker='.',linestyle='-',linewidth=1,color='b',label='potential' )
+plt.plot(dimensional_xt,yv,linestyle='-',linewidth=1,color='r',label='fourier mode' )
+plt.plot(dimensional_xt,data_fit,marker='.',linestyle='-',linewidth=1,color='g',label='fitting' )
 plt.gca().xaxis.get_major_formatter().set_powerlimits((-1, 1))
 plt.gca().yaxis.get_major_formatter().set_powerlimits((-1, 1))
 plt.xlabel(u'Time (s)')
@@ -542,25 +543,87 @@ plt.gca().legend(bbox_to_anchor = (0.0, 0.1))
 plt.tight_layout()
 plt.savefig('foo3.png')
 plt.savefig('foo3.eps')
-plt.close()
+plt.close('all')
+#plt.clf()
+
+
 
 init_plotting()
 plt.subplot(111)
 plt.gca().margins(0.1, 0.1)
-plt.plot(xf,1.0/N * np.abs(yplot),marker='.',linestyle='-',linewidth=1,color='b',label='frequency spectrum' )
+plt.plot(xf,1.0/N*np.abs(yplot),marker='.',linestyle='-',linewidth=1,color='b',label='frequency spectrum' )
 #plt.plot(xf,1.0/N*np.abs(yplotv),linestyle='-',linewidth=1,color='r',label='dominant spectrum' )
 plt.plot(xf,1.0/N*np.abs(yplotv_fit),marker='.',linestyle='-',linewidth=1,color='g',label='fitted frequency spectrum' )
+xf2lim=xf[len(xf)/2+abs( len(xf)/2-np.argmax(abs(yplotv_fit)) )*3]
 plt.gca().xaxis.get_major_formatter().set_powerlimits((-1, 1))
 plt.gca().yaxis.get_major_formatter().set_powerlimits((-1, 1))
-print plt.gca().get_ylim()
+plt.xlim(-abs(xf2lim),abs(xf2lim))
+#plt.xlim(plt.gca().get_xlim()[0]*0.1,plt.gca().get_xlim()[1]*0.1)
 plt.ylim(plt.gca().get_ylim()[0],plt.gca().get_ylim()[1]*1.3)
 plt.xlabel(u'Freq. (Hz)')
-plt.ylabel(u'|Y(freq)|')
+plt.ylabel(u'|Y[Freq.]|')
 plt.gca().legend(bbox_to_anchor = (0.1, 0.9))
 plt.tight_layout()
 plt.savefig('foo4.png')
 plt.savefig('foo4.eps')
-plt.close()
+plt.close('all')
+#plt.clf()
+
+#take log
+logy2 = np.log(y*y)
+#find zero crossing
+#zero_crossings = np.where(np.diff(np.sign(np.gradient(logy2))))[0]
+zero_crossings = np.where(np.gradient(np.sign(np.gradient(logy2)))<0 )[0]
+extremum_dimensional_xt = dimensional_xt[zero_crossings]
+extremum_logy2 = logy2[zero_crossings]
+lin_fitted_logy2 = np.polyfit(extremum_dimensional_xt,extremum_logy2,1)
+legend_lin_fitted_logy2 = 'y = %g x + %g' % (lin_fitted_logy2[0],lin_fitted_logy2[1])
+print legend_lin_fitted_logy2
+
+init_plotting()
+plt.subplot(111)
+plt.gca().margins(0.1, 0.1)
+plt.plot(dimensional_xt,logy2,linestyle='-',linewidth=1,color='b',label='ln(phi^2)' )
+plt.plot(extremum_dimensional_xt,extremum_logy2,marker='x',linewidth=1,color='g',label='extremum points' )
+plt.plot(extremum_dimensional_xt,lin_fitted_logy2[0]*extremum_dimensional_xt+lin_fitted_logy2[1],color='r',linewidth=1,label=legend_lin_fitted_logy2 )
+plt.gca().xaxis.get_major_formatter().set_powerlimits((-1, 1))
+plt.gca().yaxis.get_major_formatter().set_powerlimits((-1, 1))
+plt.xlabel(u'Time (s)')
+plt.ylabel(u'ln(|phi|^2)')
+plt.gca().legend(bbox_to_anchor = (0.1, 0.3))
+plt.tight_layout()
+plt.savefig('foo5.png')
+plt.savefig('foo5.eps')
+plt.close('all')
+#plt.clf()
+
+starting_amplitude= np.exp((extremum_logy2[-1]-lin_fitted_logy2[0]*extremum_dimensional_xt[-1])/2.0)
+#data_fit_with_growth = est_mean + starting_amplitude*np.exp(lin_fitted_logy2[0]/2.0*dimensional_xt)*np.cos(est_freq*dimensional_xt+est_phase) 
+#adjust phase
+#data_fit_with_growth_guess = est_mean + starting_amplitude*np.exp(lin_fitted_logy2[0]/2.0*dimensional_xt)*np.cos(est_freq*dimensional_xt+est_phase) 
+optimize_func = lambda z: z[1] + starting_amplitude*np.exp(lin_fitted_logy2[0]/2.0*dimensional_xt)*np.cos(est_freq*dimensional_xt+z[0]) - y
+refine_est_phase, refine_est_mean = leastsq(optimize_func, [est_phase, est_mean])[0]
+data_fit_with_growth_refine = refine_est_mean + starting_amplitude*np.exp(lin_fitted_logy2[0]/2.0*dimensional_xt)*np.cos(est_freq*dimensional_xt+refine_est_phase) 
+
+
+legend_data_fit_with_growth = r'$\gamma/\omega^*, \gamma/\omega^*_d$'+' = (%g, %g)' % ( lin_fitted_logy2[0]/2.0/omega_star, lin_fitted_logy2[0]/2.0/omega_star_point*(1.0+chi*chi))
+init_plotting()
+plt.subplot(111)
+plt.gca().margins(0.1, 0.1)
+plt.plot(dimensional_xt,y,marker='.',linestyle='-',linewidth=1,color='b',label='potential' )
+#plt.plot(dimensional_xt,data_fit_with_growth,linestyle='-',linewidth=1,color='g',label=legend_data_fit_with_growth )
+plt.plot(dimensional_xt,data_fit_with_growth_refine,linestyle='-',linewidth=1,color='r',label=legend_data_fit_with_growth )
+plt.gca().xaxis.get_major_formatter().set_powerlimits((-1, 1))
+plt.gca().yaxis.get_major_formatter().set_powerlimits((-1, 1))
+plt.xlabel(u'Time (s)')
+plt.ylabel(u'phi')
+plt.gca().legend(bbox_to_anchor = (0.0, 0.2))
+plt.tight_layout()
+plt.savefig('foo6.png')
+plt.savefig('foo6.eps')
+plt.close('all')
+#plt.clf()
+
 
 
 with open('finish.txt', 'wb') as fh:
@@ -582,16 +645,45 @@ with open('finish.txt', 'wb') as fh:
     fh.write(buf)
     buf = 'omega_star_fit/omega*_1_chi2  = %f\n'%( abs(est_freq)/omega_star_point*(1.0+chi*chi) )
     fh.write(buf)
+    buf = 'gamma  = %f\n'%(lin_fitted_logy2[0]/2.0)
+    fh.write(buf)
+    buf = 'gamma/omega*  = %f\n'%(lin_fitted_logy2[0]/2.0/omega_star)
+    fh.write(buf)
+    buf = 'gamma/omega*_1_chi2  = %f\n'%(lin_fitted_logy2[0]/2.0/omega_star_point*(1.0+chi*chi))
+    fh.write(buf)
+
+
+print "te = " , (units_temperature*electron_temperature)
+print "ti = " , (units_temperature*t0_grid_func)
+print "omega_star = " , (omega_star)
+print "omega_star_point = " , (omega_star_point)
+print "omega_star_spread = " , (omega_star_spread)
+print 'omega_star_FFT/omega*        = ', abs(freqmax)/omega_star
+print 'omega_star_FFT/omega*_point  = ', abs(freqmax)/ omega_star_point
+print 'omega_star_fit/omega*        = ',( abs(est_freq)/omega_star )
+print 'omega_star_fit/omega*_point  = ',( abs(est_freq)/omega_star_point )
+print 'omega_star_fit/omega*_spread = ',( abs(est_freq)/omega_star_spread )
+print 'omega_star_fit/omega*_point_1_chi2  = ',( abs(est_freq)/omega_star_point*(1.0+chi*chi) )
+print 'gamma  = ',(lin_fitted_logy2[0]/2.0)
+print 'gamma/omega*  = ',(lin_fitted_logy2[0]/2.0/omega_star)
+print 'gamma/omega*_1_chi2  = ',(lin_fitted_logy2[0]/2.0/omega_star_point*(1.0+chi*chi))
+
+
+
 
 
 
 import pylab
 import Image
+init_plotting('2x3')
 f = pylab.figure()
-for n, fname in enumerate(('foo1.png', 'foo2.png', 'foo3.png', 'foo4.png')):
+for n, fname in enumerate(('foo1.png', 'foo2.png', 'foo3.png', 'foo4.png', 'foo5.png','foo6.png')):
      image=Image.open(fname)#.convert("L")
      arr=np.asarray(image)
-     f.add_subplot(2, 2, n)
+     ax=f.add_subplot(2, 3, n+1)
+     ax.axis('off')
      pylab.imshow(arr)
 pylab.show()
+
+
 
