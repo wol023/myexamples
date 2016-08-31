168045af4977524e806c0290926edc9c83d978a8
Wonjae Lee <wol023@ucsd.edu>
2016-08-31 14:10:31 -0700

    x220

--- sheath/blob2D_sheath_x220t_diri_sh/runx220devel5Dopt4D.sh
+++ sheath/blob2D_sheath_x220t_diri_sh/runx220devel5Dopt4D.sh
@@ -1,1 +1,2 @@
-/home/wonjae/ws/branchinESL/devel5Dopt4D/exec/cogent.Linux.64.mpicxx.gfortran.OPT.MPI.ex  kinetic.in 
+/home/wonjae/ws/branchinESL/devel5Dopt4D/exec/cogent.Linux.64.mpicxx.gfortran.OPT.MPI.ex  kinetic.in  |tee output.txt
+
