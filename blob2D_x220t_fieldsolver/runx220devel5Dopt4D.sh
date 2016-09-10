7a86583af799d6404a3750a01e774fc322f4ca09
Wonjae Lee <wol023@ucsd.edu>
2016-09-09 17:31:31 -0700

    x220

--- blob2D_x220t_fieldsolver/runx220devel5Dopt4D.sh
+++ blob2D_x220t_fieldsolver/runx220devel5Dopt4D.sh
@@ -1,1 +1,1 @@
-/home/wonjae/ws/branchinESL/devel5Dopt4D_merging/exec/cogent.Linux.64.mpicxx.gfortran.OPT.MPI.ex  kinetic.in 
+/home/wonjae/ws/branchinESL/devel5Dopt4D/exec/cogent.Linux.64.mpicxx.gfortran.OPT.MPI.ex  kinetic.in |tee output.txt
