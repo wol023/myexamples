b6d718c47ec22f1426652664cf89d8e6b9021587
Wonjae Lee <wol023@perun-cluster.ucsd.edu>
2016-09-28 10:30:44 -0700

    perun

--- 5D_thesis/curvaturetest/blob3D_gravity_perun/perundevel5Dopt5D_256cores.sh
+++ 5D_thesis/curvaturetest/blob3D_gravity_perun/perundevel5Dopt5D_256cores.sh
@@ -1,6 +1,6 @@
 #PBS -N cogent5d
 #PBS -V
-#PBS -l nodes=4:ppn=32
+#PBS -l nodes=8:ppn=32
 #PBS -l walltime=02:00:00
 
 cd $PBS_O_WORKDIR
--- 5D_thesis/exbtest/blob3D_exb_overrideE_200Y_perun_bweno/perundevel5Dopt5D_64cores.sh
+++ 5D_thesis/exbtest/blob3D_exb_overrideE_200Y_perun_bweno/perundevel5Dopt5D_64cores.sh
@@ -1,6 +1,6 @@
 #PBS -N cogent5d
 #PBS -V
-#PBS -l nodes=4:ppn=32
+#PBS -l nodes=2:ppn=32
 #PBS -l walltime=02:00:00
 
 cd $PBS_O_WORKDIR
