378d966a9bd2c03f02463bc62cb6ec2e762518a1
wjlee1107 <wjlee1107@gmail.com>
2017-01-23 16:46:54 -0800

    perun

---
+++ 5D_thesis/curvaturetest/blob3D_nogravity_perun_regular_lowres/coridevel5Dopt5D_1024cores_regular_low.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH --partition=regular
+#SBATCH --nodes=32
+#SBATCH --qos=low
+#SBATCH --time=23:30:00
+#SBATCH --license=SCRATCH
+#SBATCH --constraint=haswell
+
+cd $SLURM_SUBMIT_DIR   # optional, since this is the default behavior
+srun -n 1024 ../../../CORIcogent5Dopt5D.ex kinetic.in
