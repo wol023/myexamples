0c07ec07cb4fd9ab5e029264b7db7ae0887f7a71
Wonjae Lee <wol023@ucsd.edu>
2017-02-27 13:25:42 -0800

    cori

--- 5D_thesis/scalingtest_nopoisson/scalingtest_singlenode_y/Helical5D_1024_4y/coridevel5Dopt5D_4cores_debug.sh
+++ 5D_thesis/scalingtest_nopoisson/scalingtest_singlenode_y/Helical5D_1024_4y/coridevel5Dopt5D_4cores_debug.sh
@@ -6,4 +6,4 @@
 #SBATCH --constraint=haswell
 
 cd $SLURM_SUBMIT_DIR   # optional, since this is the default behavior
-srun -n 4 ../../../CORIcogent5Dopt5D.ex kinetic.in
+srun -n 4 ../../../../CORIcogent5Dopt5D.ex kinetic.in
