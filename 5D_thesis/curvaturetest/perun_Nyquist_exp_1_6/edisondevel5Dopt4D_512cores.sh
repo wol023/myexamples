e22b69a1d440cc87d8963361189fb671c7afbeac
wjlee1107 <wjlee1107@gmail.com>
2017-06-16 11:50:40 -0700

    perun

---
+++ 5D_thesis/curvaturetest/perun_Nyquist_exp_1_6/edisondevel5Dopt4D_512cores.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+#SBATCH --partition debug
+#SBATCH --nodes 22
+#SBATCH --time=00:30:00
+
+cd $SLURM_SUBMIT_DIR   # optional, since this is the default behavior
+srun -n 512 ../../../EDISONcogent5Dopt4D.ex kinetic.in
